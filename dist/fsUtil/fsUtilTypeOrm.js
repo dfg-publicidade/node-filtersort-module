d0005f046d4fd80d47905822fbf68989e2782c62
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-07-04 15:11:54 -0300

    Adição do tipo ObjectId nos filtos TypeORM

--- dist/fsUtil/fsUtilTypeOrm.js
+++ dist/fsUtil/fsUtilTypeOrm.js
@@ -39,6 +39,7 @@ class FSUtilTypeOrm extends fsUtil_1.default {
                     case 'objectId': {
                         let param = params.getString(`${alias}.${field}`);
                         param = param.value ? param : params.getString(`${alias}.${field.replace(/_/ig, '')}`);
+                        param.name = '_id';
                         if (param) {
                             typeOrmQueries_1.default.inOrEq(param, qb, {
                                 filter: mongodb_1.ObjectId.isValid
